c14a0aca20704c7b29bf8034be845d845bb824d7
Alexander <ra1qcw@ra1qcw.ru>
2023-10-08 14:59:36 +0300

    Add files via upload

---
+++ nuitka-compile.bat
@@ -0,0 +1,8 @@
+D:\Python311-32\Scripts\nuitka.bat agent-wd-svc.py --standalone --onefile --remove-output --jobs=6 ^
+ --include-package=win32timezone ^
+ --include-data-files=api-ms-win-core-path-l1-1-0.dll=api-ms-win-core-path-l1-1-0.dll ^
+ --windows-service ^
+ --windows-service-name=glpi-agent-wd ^
+ --windows-service-display-name="GLPI-Agent-WD" ^
+ --windows-service-description="GLPI Agent Watchdog service" ^
+ --windows-icon-from-ico=logo.ico
